ca3fd584a40d4991e54e3903de21f1d13f14d2cd
matheus8695 <0821matheus@gmail.com>
2025-01-24 10:43:37 -0300

    LV-9: update address validations

--- tests/Feature/UserProfile/Address/UpdateTest.php
+++ tests/Feature/UserProfile/Address/UpdateTest.php
@@ -42,6 +42,77 @@ it('should be able to update a address', function () {
 });
 
 // validações de campos obrigatórios
+describe('validations', function () {
+    test('zip code rules', function ($field) {
+        Livewire::test(Update::class)
+            ->set('form.zip_code', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.zip_code' => $field->rule]);
+    })->with([
+        'required' => (object)['value' => '', 'rule' => 'required'],
+        'max'      => (object)['value' => str_repeat('*', 9), 'rule' => 'max:8'],
+    ]);
+
+    test('state id rules', function ($field) {
+        $this->artisan('app:states-sync')->assertExitCode(0);
+
+        Livewire::test(Update::class)
+            ->set('form.state_id', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.state_id' => $field->rule]);
+    })->with([
+        'required' => (object)['value' => '', 'rule' => 'required'],
+        'exists'   => (object)['value' => 30, 'rule' => 'exists:states,id'],
+    ]);
+
+    test('city rules', function ($field) {
+        Livewire::test(Update::class)
+            ->set('form.city', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.city' => $field->rule]);
+    })->with([
+        'required' => (object)['value' => '', 'rule' => 'required'],
+        'max'      => (object)['value' => str_repeat('*', 256), 'rule' => 'max:255'],
+    ]);
+
+    test('district rules', function ($field) {
+        Livewire::test(Update::class)
+            ->set('form.district', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.district' => $field->rule]);
+    })->with([
+        'required' => (object)['value' => '', 'rule' => 'required'],
+        'max'      => (object)['value' => str_repeat('*', 256), 'rule' => 'max:255'],
+    ]);
+
+    test('street rules', function ($field) {
+        Livewire::test(Update::class)
+            ->set('form.street', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.street' => $field->rule]);
+    })->with([
+        'required' => (object)['value' => '', 'rule' => 'required'],
+        'max'      => (object)['value' => str_repeat('*', 256), 'rule' => 'max:255'],
+    ]);
+
+    test('number rules', function ($field) {
+        Livewire::test(Update::class)
+            ->set('form.number', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.number' => $field->rule]);
+    })->with([
+        'required' => (object)['value' => '', 'rule' => 'required'],
+    ]);
+
+    test('complement rules', function ($field) {
+        Livewire::test(Update::class)
+            ->set('form.complement', $field->value)
+            ->call('update')
+            ->assertHasErrors(['form.complement' => $field->rule]);
+    })->with([
+        'max' => (object)['value' => str_repeat('*', 256), 'rule' => 'max:255'],
+    ]);
+});
 
 // abrir um drawer para editar o endereço
 
