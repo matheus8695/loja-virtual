91961d4ab1f85be45a9502d7c215a6ddf1181db5
matheus8695 <0821matheus@gmail.com>
2025-02-18 16:45:37 -0300

    LV-10: create a product.index route

--- tests/Feature/Product/IndexTest.php
+++ tests/Feature/Product/IndexTest.php
@@ -1,17 +1,18 @@
 <?php
 
 use App\Livewire\Product;
-use App\Models\User;
+use App\Models\{Product as ModelProduct, User};
 use Livewire\Livewire;
 
 use function Pest\Laravel\actingAs;
 
 it('should be access the product route', function () {
     /** @var User $user */
-    $user = User::factory()->create();
+    $user    = User::factory()->create();
+    $product = ModelProduct::factory()->create();
 
     actingAs($user);
 
-    Livewire::test(Product\Index::class)
+    Livewire::test(Product\Index::class, ['encodedId' => base64_encode($product->id)])
         ->assertOk();
 });
