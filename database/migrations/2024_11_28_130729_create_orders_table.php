63dbce73968d2e920534ae507657434d3a3c7a42
matheus8695 <0821matheus@gmail.com>
2025-02-24 15:53:53 -0300

    LV-11: It should ensure the user only has one open order at a time

--- database/migrations/2024_11_28_130729_create_orders_table.php
+++ database/migrations/2024_11_28_130729_create_orders_table.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\{DB, Schema};
 
 return new class () extends Migration {
     /**
@@ -17,6 +17,11 @@ return new class () extends Migration {
             $table->timestamps();
             $table->softDeletes();
         });
+
+        DB::statement('
+            CREATE UNIQUE INDEX unique_open_order_per_user ON orders (user_id)
+            WHERE status = "OPEN"
+        ');
     }
 
     /**
--- tests/Feature/Order/CreateTest.php
+++ tests/Feature/Order/CreateTest.php
@@ -58,3 +58,38 @@ test('when a user already has an open order the system have to add the product t
 
     assertDatabaseCount('product_orders', 2);
 });
+
+it('should ensure the user only has one open order at a time', function () {
+    // Cria um produto
+    $product1 = Product::factory()->create();
+
+    // Cria um pedido com status OPEN para o usuário
+    $order = ModelOrder::factory()->create([
+        'user_id' => $this->user->id,
+        'status'  => Status::OPEN,
+    ]);
+
+    // Verifica que o usuário tem apenas um pedido OPEN no banco
+    $this->assertDatabaseCount('orders', 1);
+
+    // Adiciona um produto ao pedido existente
+    Livewire::test(Order\Create::class)
+        ->call('handleProductOrder', $product1);
+
+    // Verifica novamente que o número de pedidos com status OPEN permanece 1
+    $this->assertDatabaseCount('orders', 1);
+
+    // Agora cria um segundo pedido
+    $product2 = Product::factory()->create();
+    Livewire::test(Order\Create::class)
+        ->call('handleProductOrder', $product2);
+
+    // Verifica novamente que o número de pedidos com status OPEN permanece 1
+    $this->assertDatabaseCount('orders', 1);
+
+    // Verifica que o pedido não foi duplicado
+    $this->assertDatabaseHas('orders', [
+        'user_id' => $this->user->id,
+        'status'  => Status::OPEN,
+    ]);
+});
