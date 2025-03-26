422ffc75dafb5c4d8dd4f87742887a8e47fe4598
matheus8695 <0821matheus@gmail.com>
2025-03-26 11:06:09 -0300

    LV-14: should show a message when the are no products in the order

--- tests/Feature/Order/IndexTest.php
+++ tests/Feature/Order/IndexTest.php
@@ -5,7 +5,7 @@ use App\Livewire\Order\Index;
 use App\Models\{Order, Product, User};
 use Livewire\Livewire;
 
-use function Pest\Laravel\actingAs;
+use function Pest\Laravel\{actingAs, assertDatabaseEmpty};
 
 beforeEach(function () {
     /**@var User $user */
@@ -42,4 +42,17 @@ it('should show the product data in screen', function () {
         ->test(Index::class)
         ->assertSee(implode(' ', array_slice(explode(' ', $product->title), 0, 4)))
         ->assertSee(number_format($product->price / 100, 2, ',', '.'));
+});
+
+it('should show a message when the are no products in the order', function () {
+    $order = Order::factory()->create([
+        'user_id' => $this->user->id,
+        'status'  => Status::OPEN,
+    ]);
+
+    assertDatabaseEmpty('product_orders');
+
+    Livewire::withQueryParams(['orderId' => base64_encode($order->id)])
+        ->test(Index::class)
+        ->assertSee('Não há produtos para este pedido');
 });
